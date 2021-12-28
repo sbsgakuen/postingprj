20197790ebaa82156c2dfabea5fd44a3718dd6e2
Kotani Kazumi <sbs3@kkotani.net>
2021-12-28 23:05:42 +0900

    2021.12.28-1

--- postingprj/settings.py
+++ postingprj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postingapp.apps.PostingappConfig',    
-    'django_bootstrap5',
+   # 'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
