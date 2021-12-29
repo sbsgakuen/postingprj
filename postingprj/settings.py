313e9af9380651a2f19e47f0300fed3ba22f8249
Kotani Kazumi <sbs3@kkotani.net>
2021-12-29 21:56:00 +0900

    first commit

--- postingprj/settings.py
+++ postingprj/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postingapp.apps.PostingappConfig',    
-   # 'django_bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -131,4 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/postingapp/login/'
